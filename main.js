6cf345c6296dff22aa14e16471ee62257bcafd84
RasKids <RasKids@macs-MacBook-Pro-2.local>
2020-10-27 08:05:29 -0600

    dark mode

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.querySelector(".course").style.backgroundColor = "#111";
+    document.querySelector("main").style.backgroundColor = "#000";
+    document.querySelector("nav").style.backgroundColor = "#111";
+    document.querySelector(".title").style.color = "#fff";
+}
+window.matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', event => {
+        if (event.matches) {
+            document.querySelector(".course").style.backgroundColor = "#111";
+            document.querySelector("main").style.backgroundColor = "#000";
+            document.querySelector("nav").style.backgroundColor = "#111";
+            document.querySelector(".title").style.color = "#fff";
+        } else {
+            document.querySelector(".course").style.backgroundColor = "##ff5252";
+            document.querySelector("main").style.backgroundColor = "#eee";
+            document.querySelector("nav").style.backgroundColor = "#3f51b5";
+            document.querySelector(".title").style.color = "#000";
+        }
+    })
+
 let http = new XMLHttpRequest();
 http.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
