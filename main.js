0c7b00985782f43de401edafdc1049b7046b7dc5
RasKids <RasKids@macs-MacBook-Pro-2.local>
2020-10-27 09:40:51 -0600

    more

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
         let course = document.querySelectorAll(".course")[i]
         course.style.background = "url('background2.jpg')";
         course.style.backgroundAttachment = "fixed";
-        course.style.position = "center";
-        course.style.repeat = "no-repeat";
+        course.style.backgroundPosition = "center";
+        course.style.backgroundRepeat = "no-repeat";
         course.style.backroundSize = "cover";
+        // console.log(i)
     }
     document.querySelector("main").style.background = "#000";
     document.querySelector("nav").style.background = "#111";
@@ -18,9 +19,10 @@ window.matchMedia('(prefers-color-scheme: dark)')
                 let course = document.querySelectorAll(".course")[i]
                 course.style.background = "url('background2.jpg')";
                 course.style.backgroundAttachment = "fixed";
-                course.style.position = "center";
-                course.style.repeat = "no-repeat";
+                course.style.backgroundPosition = "center";
+                course.style.backgroundRepeat = "no-repeat";
                 course.style.backroundSize = "cover";
+                console.log(i)
             }
             document.querySelector("main").style.background = "#000";
             document.querySelector("nav").style.background = "#111";
@@ -30,9 +32,10 @@ window.matchMedia('(prefers-color-scheme: dark)')
                 let course = document.querySelectorAll(".course")[i]
                 course.style.background = "url('background3.jpg')";
                 course.style.backgroundAttachment = "fixed";
-                course.style.position = "center";
-                course.style.repeat = "no-repeat";
+                course.style.backgroundPosition = "center";
+                course.style.backgroundRepeat = "no-repeat";
                 course.style.backroundSize = "cover";
+                console.log(i)
             }
             document.querySelector("main").style.background = "#eee";
             document.querySelector("nav").style.background = "#3f51b5";
@@ -45,7 +48,7 @@ http.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         let info = JSON.parse(this.responseText);
         for (let i = 0; i <= 2; i++) {
-            console.log(i);
+            // console.log(i);
             let target = document.querySelector(`.c${i}`);
             target.querySelector("img").src = info.courses[i].image;
             target.querySelector("p").innerHTML = info.courses[i].name;
